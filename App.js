3c3a83b59b7c409812ff1a836b475f03485a8c73
Can Kanbur <cankanbur1@gmail.com>
2022-10-26 11:34:32 +0300

    move some folder

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 
 
