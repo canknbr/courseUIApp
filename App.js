a721a645347fd0639486e3cb7f42945a2e38015d
Can Kanbur <cankanbur1@gmail.com>
2022-10-26 11:33:24 +0300

    .

--- App.js
+++ App.js
@@ -4,15 +4,10 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Icon from 'react-native-vector-icons/Entypo';
-const myIcon = <Icon name="rocket" size={30} color="#000" />;
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Hello {myIcon} </Text>
-    </View>
-  );
-}
+
+import HomeScreen from './screens/HomeScreen';
+
+
 
 const Stack = createNativeStackNavigator();
 
